9c373cd6284aa0191479f2ef6259fefad1d25163
JplsMura <joaopedrolima.s@hotmail.com>
2025-03-20 12:57:49 -0300

    Polimorfismo em tempo de compilação

---
+++ primeiro-projeto/src/polimorfismo/Principal.java
@@ -0,0 +1,21 @@
+package polimorfismo;
+
+public class Principal {
+    public static void main(String[] args) {
+        int sabaoPo = 1;
+        int sabonente = 2;
+        double arroz = 5.90;
+        double mamao = 2.50;
+        Integer cenoura = 6;
+        Integer batata = 7;
+        Integer banana = 7;
+
+        int total = Calculadora.somar(sabaoPo, sabonente);
+        double totalDouble = Calculadora.somar(arroz, mamao);
+        Integer totalInteger = Calculadora.somar(cenoura, batata, banana);
+
+        System.out.println("Calculadora int " + total);
+        System.out.println("Calculadora double " + totalDouble);
+        System.out.println("Calculadora Integer " + totalInteger);
+    }
+}
