00a07119182af133edf03b2ec342704f8e37d478
JplsMura <joaopedrolima.s@hotmail.com>
2025-03-24 14:25:34 -0300

    Exception com java e tratamentos personalizados

---
+++ primeiro-projeto/src/excecao/naoverificada/VerificaCPF.java
@@ -0,0 +1,27 @@
+package excecao.naoverificada;
+
+import excecao.ConflictException;
+
+import java.util.Scanner;
+
+public class VerificaCPF {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu cpf ");
+        String verificaCPF = verificaCPF(scanner.next());
+        System.out.println(verificaCPF);
+    }
+
+    public static String verificaCPF(String cpf) {
+        try {
+            if (cpf.equals("000123400045")) {
+                throw new ConflictException("CPF já cadastrado " + cpf);
+            } else {
+                return "CPF válido, usuário cadastrado";
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("CPF já cadastrado " + cpf);
+        }
+    }
+}
